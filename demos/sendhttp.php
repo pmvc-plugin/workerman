ef3e266a0dcb59d4025fad749cf31d9b8353c866
Hill <hill@kimo.com>
2022-02-15 11:13:54 +0000

    improve demo

--- demos/sendhttp.php
+++ demos/sendhttp.php
@@ -4,5 +4,11 @@ include_once "../vendor/autoload.php";
 \PMVC\Load::plug();
 \PMVC\addPlugInFolders(["../../"]);
 
+$oPHPUnit = \PMVC\plug("dev")->phpunit("workerman");
+
 $ws = \PMVC\plug("workerman");
 $ws->sendHttp("jjjjjjj");
+
+$array = $oPHPUnit->toArray();
+
+print_r($array);
